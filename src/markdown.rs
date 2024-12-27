e7c996e4ea7715d60ced53a97621435b3ef9d5ea
spatten <scott@fossa.com>
2024-12-26 18:48:25 -0800

    blow up if there is anything before the frontmatter

--- src/markdown.rs
+++ src/markdown.rs
@@ -6,6 +6,8 @@ type Frontmatter = toml::Value;
 pub enum Error {
     #[error("invalid frontmatter")]
     InvalidFrontmatter(String),
+    #[error("content before frontmatter")]
+    ContentBeforeFrontmatter(String),
 }
 
 pub struct Page {
@@ -17,13 +19,20 @@ pub fn parse(markdown: &str) -> Result<Page, Error> {
     let mut sections = markdown.split("+++");
     // If there are less than 3 sections, there is no frontmatter,
     // so we just return the whole thing as the body.
-    // If there are more than 3 sections, there is frontmatter,
+    // If there are three or more sections, there is frontmatter,
     // so we parse the frontmatter and the body.
+    // any further +++ lines are just part of the body
     let frontmatter: Frontmatter = if sections.clone().count() < 3 {
         toml::from_str("").expect("empty frontmatter should be valid")
     } else {
         // get rid of the first `+++` line
-        let _ = sections.next().unwrap();
+        let before = sections.next().expect("should have at least 3 sections");
+        if !before.trim().is_empty() {
+            return Err(Error::ContentBeforeFrontmatter(format!(
+                "content before frontmatter: {}",
+                before
+            )));
+        }
         let frontmatter_content = sections.next().unwrap();
         toml::from_str(frontmatter_content).map_err(|e| Error::InvalidFrontmatter(e.to_string()))?
     };
--- tests/it/markdown.rs
+++ tests/it/markdown.rs
@@ -21,6 +21,22 @@ use simple_test_case::test_case;
     "<h1>Hello, world!</h1>\n");
     "moderate frontmatter and post"
   )]
+#[test_case(
+  r#"
+
+
+
+
+  +++
+  title = "Hello, world!"
+  +++
+  # Hello, world!
+"#,
+  (
+    r#"title = "Hello, world!""#,
+    "<h1>Hello, world!</h1>\n");
+    "whitespace before frontmatter"
+  )]
 #[test_case("# Hello, world!", ("", "<h1>Hello, world!</h1>\n"); "no frontmatter")]
 #[test_case("+++\ntitle = \"Hello, world!\"\n+++\n# Hello, world!\n+++\ncontinuing", ("title = \"Hello, world!\"", "<h1>Hello, world!</h1>\n<p>+++</p>\n<p>continuing</p>\n"); "multiple plus-plus-plus lines")]
 #[test]
@@ -31,7 +47,8 @@ fn test_parse_with_valid_frontmatter(markdown: &str, expected: (&str, &str)) {
 }
 
 #[test_case(
-    "+++\ntitle+++\n# Hello, world!\n+++\ncontinuing"; "invalid frontmatter")]
+    "+++\ntitle+++\n# Hello, world!\n+++\ncontinuing"; "invalid toml in frontmatter") ]
+#[test_case("before the frontmatter\n+++\ntitle = \"Hello, world!\"\n+++\n# Hello, world!\n+++\ncontinuing"; "content before frontmatter")]
 #[test]
 fn test_parse_with_invalid_frontmatter(markdown: &str) {
     let res = markdown::parse(markdown);

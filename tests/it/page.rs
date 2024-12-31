10d08f736bef029f16de44c390a7388ff41899a6
spatten <scott@fossa.com>
2024-12-30 20:05:49 -0800

    get tests working again

--- tests/it/page.rs
+++ tests/it/page.rs
@@ -8,29 +8,33 @@ use tera::Tera;
 #[test_case(
   r#"
   <h1>{{title}}</h1>
-  <div class="post">
+  <div class="page">
   {{body|safe}}
   </div>
   "#,
   Path::new("pages/index.md"),
   r#"
+  +++
+  title = "Home Page"
+  +++
+  This is my home page.
+  "#,
+  r#"
   <h1>Home Page</h1>
-  <div class="post">
+  <div class="page">
   <p>This is my home page.</p>
 
   </div>
   "#
 ; "simple frontmatter and post")]
 #[test]
-fn test_page_from_content(layout: &str, input_path: &Path, expected: &str) {
+fn test_page_from_content(layout: &str, input_path: &Path, content: &str, expected: &str) {
     let mut tera = Tera::default();
     let output_dir = env::temp_dir();
-    let root_dir = Path::new("tests/it/testdata/simple_site");
     tera.add_raw_template("default.html", layout)
         .expect("should be able to add template");
-    let page = page::Page::from_file(root_dir, input_path)
-        .expect("should parse")
-        .unwrap();
+    let page_file = page::PageFile::try_from(input_path.to_path_buf()).expect("should parse");
+    let page = page::Page::from_content(page_file, content).expect("should parse");
     let posts = vec![];
     page.render(&tera, &output_dir, &posts)
         .expect("should render");

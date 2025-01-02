a13679088d4000ae27a390227da2b19c9bd447b5
spatten <scott@fossa.com>
2025-01-01 20:20:15 -0800

    remove comments

--- src/watch.rs
+++ src/watch.rs
@@ -6,15 +6,9 @@ use std::{path::Path, sync::mpsc};
 pub fn watch(input_dir: &Path, output_dir: &Path) -> Result<(), Error> {
     let (tx, rx) = mpsc::channel::<Result<Event, notify::Error>>();
 
-    // Use recommended_watcher() to automatically select the best implementation
-    // for your platform. The `EventHandler` passed to this constructor can be a
-    // closure, a `std::sync::mpsc::Sender`, a `crossbeam_channel::Sender`, or
-    // another type the trait is implemented for.
     let mut watcher =
         notify::recommended_watcher(tx).unwrap_or_else(|e| panic!("notify error: ${e}"));
 
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
     watcher.watch(input_dir, RecursiveMode::Recursive)?;
     for result in rx {
         match result {

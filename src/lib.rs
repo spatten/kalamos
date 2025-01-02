6c90568f5b78b5af873514887b6a5306cbfe6ac4
spatten <scott@fossa.com>
2025-01-01 20:07:49 -0800

    move watch into lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod parser;
 pub mod post;
 pub mod render;
 pub mod serve;
+pub mod watch;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use clap::{Parser, Subcommand};
-use kalamos::{render, serve};
+use kalamos::{render, serve, watch};
 use log::info;
-use notify::{Event, RecursiveMode, Watcher};
-use std::{path::PathBuf, sync::mpsc};
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -90,33 +89,9 @@ fn main() {
                 "Watching {:?} and outputting to {:?}",
                 input_dir, output_dir
             );
-            let (tx, rx) = mpsc::channel::<Result<Event, notify::Error>>();
-
-            // Use recommended_watcher() to automatically select the best implementation
-            // for your platform. The `EventHandler` passed to this constructor can be a
-            // closure, a `std::sync::mpsc::Sender`, a `crossbeam_channel::Sender`, or
-            // another type the trait is implemented for.
-            let mut watcher =
-                notify::recommended_watcher(tx).unwrap_or_else(|e| panic!("notify error: ${e}"));
-
-            // Add a path to be watched. All files and directories at that path and
-            // below will be monitored for changes.
-            watcher.watch(&input_dir, RecursiveMode::Recursive).unwrap();
-            for result in rx {
-                match result {
-                    Ok(event) => {
-                        // deal with case where the output directory is a subdirectory of the input directory
-                        if event.paths.iter().all(|p| p.starts_with(&output_dir)) {
-                            continue;
-                        }
-                        info!("change event: {:?}", event);
-                        render::render_dir(&input_dir, &output_dir).unwrap_or_else(|e| {
-                            info!("Error rendering posts and pages: {}", e);
-                        });
-                    }
-                    Err(e) => info!("change event error: {:?}", e),
-                }
-            }
+            watch::watch(&input_dir, &output_dir).unwrap_or_else(|e| {
+                panic!("Error watching: {:?}", e);
+            });
         }
         Commands::New { name, template } => {
             info!("New site: {:?}, template: {:?}", name, template);

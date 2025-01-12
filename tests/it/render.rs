37d72e5e0eb39c22d2c0b08c883b2908011f69e1
spatten <scott@fossa.com>
2025-01-11 21:01:23 -0800

    no need to panic if tmpdir does not already exist

--- tests/it/render.rs
+++ tests/it/render.rs
@@ -8,7 +8,7 @@ fn test_render_dir() {
     let temp_dir = env::temp_dir();
     let root_dir = Path::new("tests/it/testdata/simple_site");
     let output_dir = temp_dir.join("kalamos_test_output");
-    fs::remove_dir_all(&output_dir).expect("should remove output dir");
+    let _ = fs::remove_dir_all(&output_dir);
     render::render_dir(root_dir, &output_dir).expect("should render");
     let output_content = support::dir_to_yaml(&output_dir).expect("should generate yaml");
     assert_yaml_snapshot!(output_content);
